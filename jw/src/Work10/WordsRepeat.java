cc29c716b2d3d7f4e4dbcdcd669d8dc4b4fb4779
smeshnoi <sergey.zinkevich@gmail.com>
2017-10-15 09:37:20 +0300

    Work10_1

---
+++ jw/src/Work10/WordsRepeat.java
@@ -0,0 +1,43 @@
+package Work10;
+
+/**
+ * Created by User on 12.10.2017.
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class WordsRepeat {
+    public static void main(String[] args) {
+        String text = "Some words are, are, Are  repeated more more often Often than other words!";
+        List<String> list = new ArrayList<>();
+        int i = 0;
+        for (int j = 0; j < text.length(); j++) {
+            if (text.charAt(j) == ' ' || text.charAt(j) == '.' || text.charAt(j) == '!' || text.charAt(j) == '?' || text.charAt(j) == ',') {
+                if (j > i) {
+                    list.add(text.substring(i, j).trim());
+                }
+                i = j + 1;
+            }
+        }
+        System.out.println(text + "\n");
+        HashMap<String, Integer> wordsRepeat = new HashMap<>();
+        String temp;
+        int count;
+        for (i = 0; i < list.size(); i++) {
+            count = 0;
+            temp = list.get(i);
+            for (String value: list) {
+                if (temp.equals(value)) {
+                    count++;
+                }
+            }
+            wordsRepeat.put(temp, count);
+        }
+        for (String value: wordsRepeat.keySet()) {
+            System.out.println(value + " - " + wordsRepeat.get(value));
+        }
+    }
+}
